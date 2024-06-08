2cceb35b6aeb6a205560a058318f70682b307ef0
gysagsohn <gysagsohn@hotmail.com>
2024-06-08 11:13:46 +1000

    feature testing

--- src/server.js
+++ src/server.js
@@ -7,23 +7,18 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (request, response, next) => {
-
-	return next(new Error("Error on purpose from root route"))
-
-	response.json({
-		message:"Hello world!"
-	});
-});
+const apiV2 = require("./v2/index.js");
+app.use("/v2", apiV2);
 
 app.use((error, request, response, next) => {
+    console.log(error.message);
 	response.json({
 		message:"Error occured!",
-		error: error
+		error: error.message
 	});
 });
 
 
-app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
-});
+
+
+module.exports = {app, port}
---
+++ test/server.test.js
@@ -0,0 +1,46 @@
+
+const request = require('supertest');
+const {app} = require('../src/server.js');
+
+describe("Home page route shows an error", () => {
+
+	it("Server returns an object with a message and error property", async () => {
+		// localhost:3000/v2/
+		const response = await request(app).get("/v2/");
+		console.log(response.body);
+		expect(response.body.message).toBeTruthy();
+	});
+});
+
+describe("v2/ functionality", () => {
+
+	it("v2/ POST receives data correctly", async () => {
+		const response = await 
+		request(app)
+		.post("/v2/")
+		.send({
+			movie: "Dune"
+		});
+
+		/*
+		Expected response body:
+		{
+			message:"Received data!",
+			data: {
+				movie: "Dune"
+			}
+		}
+		*/
+
+		expect(response.body.data.movie).toBe("Dune");
+		expect(response.body.data.movie).toBeTruthy();
+	})
+
+	it("v2/headercheck GET receives an auth header correctly", async () => {
+		const response = await request(app).get("/v2/headercheck").auth("alex was here", {type: "bearer"});
+
+		expect(response.body.data).toBe("Bearer alex was here");
+	});
+
+});
+
